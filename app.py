b191b88d59018b881f4b419ad1b08ed117a54e58
Emily Davis <edavis05@terpmail.umd.edu>
2026-09-10 16:00:28 -0400

    Changed text to include second phrase

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Emily Davis!'
+    return "Hello World from Emily Davis. This is my first code change."
 
 @app.route('/hello')
 def hello():
